c15a66c871b1097b4db98e3a8dee5590085ef5c9
shanelperera <shanelradperera@gmail.com>
2025-03-22 12:45:03 +0530

    Implement a circular queue

--- Queue/circular.c
+++ Queue/circular.c
@@ -5,7 +5,10 @@
 int queue[MAX];
 int front = -1, rear = -1;
 
-void insert(int c)
+void insert(void);
+void display(void);
+int delete(void);
+int peek(void);
 
 int main(void) {
     int option, val;
@@ -43,9 +46,11 @@ int main(void) {
     return 0;
 }
 
-void insert(int c) {
+void insert(void) {
     int num;
-    
+    printf("Enter the number to be inserted into the queue: ");
+    scanf("%d", &num);
+
     if (front == 0 && rear == MAX - 1) {
         printf("OVERFLOW\n");
     }
@@ -53,7 +58,7 @@ void insert(int c) {
         front = rear = 0;
         queue[rear] = num;
     }
-    else if (rear = MAX -1 && front != 0) {
+    else if (rear == MAX -1 && front != 0) {
         rear = 0;
         queue[rear] = num;
     }
@@ -63,3 +68,47 @@ void insert(int c) {
     }
 }
 
+int delete(void) {
+    int val;
+    if (front == -1 && rear == -1) {
+        printf("UNDERFLOW\n");
+        return -1;
+    }
+
+    val = queue[front];
+    if (front == rear) {
+        front = rear = -1;
+    }
+    else {
+        if (front == MAX -1) {
+            front = 0;
+        }
+        else {
+            front++;
+        }
+    }
+    return val;
+}
+
+int peek(void) {
+    if (front == -1 && rear == -1) {
+        printf("QUEUE IS EMPTY\n");
+        return -1;
+    }
+    else {
+        return queue[front];
+    }
+}
+
+void display(void) {
+    if (front == -1 || front > rear) {
+        printf("QUEUE IS EMPTY\n");
+    }
+    else {
+        printf("Queue: ");
+        for (int i = front; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+        printf("\n");
+    }
+}
