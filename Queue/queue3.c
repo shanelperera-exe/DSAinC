951cfa00843bcd5f4a05d4925720a916faf1fc81
shanelperera <shanelradperera@gmail.com>
2025-04-15 12:53:03 +0530

    Refactor queue implementation for improved structure and functionality, including streamlined enqueue and dequeue operations, and enhanced main menu interface.

--- Queue/queue3.c
+++ Queue/queue3.c
@@ -1,27 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
+struct node {
     int data;
     struct node *next;
-} node;
+};
 
-typedef struct queue {
-    node *front;
-    node *rear;
-} queue;
+struct queue {
+    struct node *front;
+    struct node *rear;
+};
 
-void create_queue(queue *q);
-void *insert(queue *q, int num);
-void *delete(queue *q);
-int peek(queue *q);
-void display_queue(queue *q);
+struct queue *q;
+
+struct queue *enqueue(struct queue *q, int val) {
+    struct node *ptr;
+    ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = val;
+    if (q->front == NULL) {
+        q->front = q->rear = ptr;
+        q->front->next = q->rear->next = NULL;
+    }
+    else {
+        q->rear->next = ptr;
+        q->rear = ptr;
+        q->rear->next = NULL;
+    }
+    return q;
+}
+
+struct queue *dequeue(struct queue *q) {
+    struct node *ptr;
+    ptr = q->front;
+    if (q->front == NULL) {
+        printf("UNDERFLOW\n");
+    }
+    else {
+        q->front = q->front->next;
+        printf("Value deleted from the queue: %d\n", ptr->data);
+        free(ptr);
+    }
+    return q;
+}
+
+int peek(struct queue *q) {
+    if (q->front == NULL) {
+        printf("QUEUE IS EMPTY.\n");
+    }
+    else {
+        return q->front->data;
+    }
+}
+
+void display_queue(struct queue *q) {
+    struct node *ptr;
+    ptr = q->front;
+    if (q->front == NULL) {
+        printf("QUEUE IS EMPTY.\n");
+        return;
+    }
+
+    printf("Queue: ");
+    while (ptr != NULL) {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
 
 int main(void) {
-    queue q;
-    create_queue(&q);
-
     int option, val;
+    q = (struct queue *)malloc(sizeof(struct queue));
+    q->front = q->rear = NULL;
+
     do {
         printf("\n***** MAIN MENU *****\n");
         printf("[1] Insert an element\n");
@@ -34,83 +85,35 @@ int main(void) {
 
         switch(option) {
             case 1:
-                printf("Enter the number to insert: ");
+                printf("Enter the value to insert: ");
                 scanf("%d", &val);
-                insert(&q, val);
+                q = enqueue(q, val);
                 break;
             case 2:
-                delete(&q);
+                q = dequeue(q);
                 break;
             case 3:
-                val = peek(&q);
-                if (val != -1) {
-                    printf("The first number in the queue: %d\n", val);
+                val = peek(q);
+                if (q->front != NULL) {
+                    printf("The first value in the queue: %d\n", val);
                 }
                 break;
             case 4:
-                display_queue(&q);
+                display_queue(q);
                 break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid option! Please try again.\n");
         }
     } while(option != 5);
-    
+
+    // Free the queue memory
+    while (q->front != NULL) {
+        q = dequeue(q);
+    }
+    free(q);
+
     return 0;
-}
-
-void create_queue(queue *q) {
-    q->rear = NULL;
-    q->front = NULL;
-}
-
-void *insert(queue *q, int num) {
-    node *ptr;
-    ptr = (node*) malloc(sizeof(node));
-    ptr->data = num;
-
-    if (q->front == NULL) {
-        q->front = q->rear = ptr;
-        q->front->next = q->rear->next = NULL;
-    }
-    else {
-        q->rear->next = ptr;
-        q->rear = ptr;
-        q->rear->next = NULL; 
-    }
-}
-
-void *delete(queue *q) {
-    node *ptr;
-    ptr = q->front;
-
-    if (q->front == NULL) {
-        printf("UNDERFLOW\n");
-    }
-    else {
-        q->front = q->front->next;
-        printf("The number being deleted: %d\n", ptr->data);
-        free(ptr);
-    }
-}
-
-int peek(queue *q) {
-    if (q->front == NULL) {
-        printf("QUEUE IS EMPTY\n");
-        return -1;
-    } 
-    else {
-        return q->front->data;
-    }
-}
-
-void display_queue(queue *q) {
-    node *ptr = q->front;
-    if (ptr == NULL) {
-        printf("QUEUE IS EMPTY\n");
-        return;
-    }
-    printf("Queue: ");
-    while (ptr != NULL) {
-        printf("%d -> ", ptr->data);
-        ptr = ptr->next;
-    }
-    printf("NULL\n");
 }
